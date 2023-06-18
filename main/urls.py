48a00b19b8654401a049c179a1d46efd35fe78d4
aayush.koi <67232697+aayushkoi@users.noreply.github.com>
2023-06-18 16:01:59 -0400

    test

--- main/urls.py
+++ main/urls.py
@@ -11,6 +11,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('access-table/<int:manga_id>/', access_table, name='access_table'),
     path('access-top-100/', views.access_top_100, name='access_top_100'),
+    path('access-top-seinen/', views.acces_top_seinen, name='acces_top_seinen'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
