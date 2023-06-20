f83c98daa10e847c4ebfc5e13c921e728f84f4a8
kmishra3 <kushal.mishra781@gmail.com>
2023-06-20 19:19:15 -0400

    fixing push

---
+++ mysite/main/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from django.urls import path, include
+from .views import access_table
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
+urlpatterns = [
+    path('access-table/<int:manga_id>/', access_table, name='access_table'),
+    path('access-top-100/', views.access_top_100, name='access_top_100'),
+    path('access-top-shounen/', views.access_top_shounen, name="access_top_shounen"),
+    path('access-top-manhua/', views.access_top_manhua, name="access_top_manhua"),
+    path('access-top-shoujo/', views.access_top_shoujo, name="access_top_shoujo"),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
