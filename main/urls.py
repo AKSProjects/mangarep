d9437c5c40d28da5a5415c224102ce87fd4b3f24
aayush.koi <67232697+aayushkoi@users.noreply.github.com>
2023-07-04 19:57:16 -0400

    grid changes by kushal with sidevan and manga_template.html change by aayush

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('manga/<int:manga_id>/', views.manga_template, name='manga_detail'),
     path('', views.home, name='home'),
     path('access-table/<int:manga_id>/', access_table, name='access_table'),
     path('access-top-100/', views.access_top_100, name='access_top_100'),
